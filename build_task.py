4618a15bd767ec3c4a9bed59976c2c3874db6418
Михаил А. Похилый <m.pohilyj@pohilyj-ws.officeru.4slovo.ru>
2020-01-30 13:18:30 +0300

    refactor: теперь slov -> rc в таблице

--- build_task.py
+++ build_task.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from re import findall
 
 import configparser
 from jira import JIRA
@@ -7,6 +8,7 @@ import requests
 from send_notifications import RELEASE_ISSUES_URL, ISSUE_URL, RELEASES_LIST_URL, RELEASE_URL, REMOTE_LINK, GIT_LAB, STATUS_FOR_RELEASE
 from send_notifications import get_issues
 
+SHOW_SLOV_MERGES = True
 
 def get_merge_requests(issue_number):
     """ Ищет ссылки на мердж реквесты в задаче и возвращает список ссылок """
@@ -17,20 +19,19 @@ def get_merge_requests(issue_number):
         url = link['object']['url']
         if GIT_LAB in url:
             result.add(url)
-    return result
+    return list(result)
 
 
-def sort_merge_requests(urls):
-    """ Возвращает словарь мерджреквестов, рассортированных по проектам """
+def sort_merge_requests(links):
+    """ Возвращает словарь списков мерджреквестов, рассортированных по проектам """
     projects = defaultdict(list)
-    for url in urls:
-        url_parts = url.split('/')
-        if 'commit' in url:
-            continue
-        if 'docker' not in url:
-            projects[url_parts[4]].append(url)
+    for link in links:
+        link = link[0]
+        url_parts = link.split('/')
+        if 'docker' not in link:
+            projects[url_parts[4]].append(link)
         else:
-            projects['docker'].append((url))
+            projects['docker'].append(link)
     return projects
 
 
@@ -47,6 +48,14 @@ def get_release_id(config):
     raise Exception('Release not found')
 
 
+def get_links(merges):
+    """ возвращает ссылки на мерджи SLOV -> RC для таблицы """
+    result = []
+    for link in merges:
+        result.append(f'<a href="{link}" class="external-link" rel="nofollow">{link}</a><br/>')
+    return ''.join(result)
+
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -58,10 +67,12 @@ if __name__ == '__main__':
     #           До таблицы
     #
     message = f"<p><b>Состав релиза:</b></p><p><a href='{RELEASE_URL.format(release_info['id'])}' " \
-           f"class='external-link' rel='nofollow'>{RELEASE_URL.format(release_info['id'])}</a></p>" \
-           f"<div class='table-wrap'><table class='confluenceTable'><tbody>"\
-           f"<tr><th class='confluenceTh'>№</th><th class='confluenceTh'>Задача</th>" \
-           f"<th class='confluenceTh'>Подлит свежий мастер, нет конфликтов</th></tr>"
+              f"class='external-link' rel='nofollow'>{RELEASE_URL.format(release_info['id'])}</a></p>" \
+              f"<div class='table-wrap'><table class='confluenceTable'><tbody>" \
+              f"<tr><th class='confluenceTh'>№</th>" \
+              f"<th class='confluenceTh'>Задача</th>" \
+              f"<th class='confluenceTh'>SLOV -&gt; RC</th>" \
+              f"<th class='confluenceTh'>Подлит свежий мастер, нет конфликтов</th></tr>"
     #
     #           Выбираем задачи для релиза в нужных статусах
     #
@@ -69,59 +80,61 @@ if __name__ == '__main__':
         if 'сборка' not in issue['fields']['summary'].lower() and issue['fields']['status']['name'] in STATUS_FOR_RELEASE:
             issues_list[issue['key']] = issue['fields']['summary']
     #
+    #           Собираем мердж реквесты
+    #
+    merge_requests = defaultdict(list)
+    if issues_list:
+        for issue_number in issues_list:
+            for merge in get_merge_requests(issue_number):
+                if 'commit' in merge:
+                    continue
+                merge_requests[issue_number].append(merge)
+    projects = sort_merge_requests(merge_requests.values())
+    #
     #           Заполняем таблицу
     #
-    merge_requests = set()
-    if issues_list:
-        for number, issue_number in enumerate(sorted(issues_list)):
-            merge_requests |= (get_merge_requests(issue_number))
-            message += f"<tr><td class='confluenceTd'>{number + 1}</td>" \
-                       f"<td class='confluenceTd'><a href='{ISSUE_URL}{issue_number}" \
-                       f"'title='{issues_list[issue_number]}' class='issue-link' data-issue-key='{issue_number}'" \
-                       f">{issue_number}</a></td><td class='confluenceTd'></td></tr>"
-        projects = sort_merge_requests(merge_requests)
-        message += '</tbody></table></div>'
-        #
-        #           Docker -> Master
-        #
-        if 'docker' in projects:
-            docker = True
-            message += '<p><b>Docker -&gt; Master</b></p>'
-            for merge_request in projects['docker']:
-                message += f'<p><a href="{merge_request}" class="external-link" rel="nofollow">{merge_request}</a> </p>'
-        #
-        #           SLOV -> RC
-        #
-        message += '<p></p><p><b>SLOV -&gt; RC</b></p><p></p><p></p>'
-        for project in projects:
-            if project == 'docker':
-                continue
-            for merge_request in projects[project]:
-                message += f'<p><a href="{merge_request}" class="external-link" rel="nofollow">{merge_request}</a> </p>'
-            message += '<br/>'
-        #
-        #           RC -> Staging
-        #
-        message += '<p></p><p><b>RC -&gt; Staging</b></p><p></p><p></p>'
-        #
-        #           Staging -> Master
-        #
-        message += '<p></p><p></p><p><b>Staging -&gt; Master</b></p><p></p>'
-        #
-        #           Staging -> Master
-        #
-        message += '<p></p>'
-        #
-        #           Вывод результата в Jira
-        #
-        html = f"""{message}"""
-        with open('message.html', 'w') as file:
-            file.write(html)
+    for index, issue_number in enumerate(sorted(issues_list)):
+        message += f"<tr><td class='confluenceTd'>{index + 1}</td>" \
+                   f"<td class='confluenceTd'><a href='{ISSUE_URL}{issue_number}'title='{issues_list[issue_number]}' " \
+                   f"class='issue-link' data-issue-key='{issue_number}'>{issue_number}</a></td>" \
+                   f"<td class='confluenceTd'>{get_links(merge_requests[issue_number])}</td>" \
+                   f"<td class='confluenceTd'>ок</td></tr>"
+    message += '</tbody></table></div>'
+    #
+    #           Создаем RC
+    #
+    pass
+    #
+    #           Docker -> Master
+    #
+    if 'docker' in projects:
+        docker = True
+        message += '<p><b>Docker -&gt; Master</b></p>'
+        for merge_request in projects['docker']:
+            message += f'<p><a href="{merge_request}" class="external-link" rel="nofollow">{merge_request}</a> </p>'
+    #
+    #           RC -> Staging
+    #
+    message += '<p></p><p><b>RC -&gt; Staging</b></p><p></p><p></p>'
+    #
+    #           Staging -> Master
+    #
+    message += '<p></p><p></p><p><b>Staging -&gt; Master</b></p><p></p>'
+    #
+    #           Staging -> Master
+    #
+    message += '<p></p>'
+    #
+    #           Вывод результата в Jira
+    #
+    html = f"""{message}"""
+    with open('message.html', 'w') as file:
+        file.write(html)
 
 
 
-        # добавить задачу в жиру, добавить сборочную задачу в релиз
-        # сделать RC ветки
-        # деплойные действия
-        # развернуть стенд на нужных ветках и запустить тесты в гитлабе и регресс (в Дженкинс?)
+    # добавить задачу в жиру, добавить сборочную задачу в релиз
+    # сделать RC ветки
+    # деплойные действия
+    # развернуть стенд на нужных ветках и запустить тесты в гитлабе и регресс (в Дженкинс?)
 
