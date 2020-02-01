5a751a6e8df77868ec31ff06aef62bd60090e6a8
Michael Pohily <mpohily@gmail.com>
2020-02-01 17:01:08 +0300

    start merge_requests module

--- merge_requests.py
+++ merge_requests.py
@@ -1,2 +1,14 @@
 import gitlab
+from configparser import ConfigParser
+
+
+if __name__ == '__main__':
+    config = ConfigParser()
+    config.read('config.ini')
+    gl = gitlab.Gitlab('https://gitlab.4slovo.ru/', private_token=config['user_data']['GITLAB_PRIVATE_TOKEN'])
+    projects = gl.projects.list()
+    python_scripts = projects[3]
+    branches = python_scripts.branches.list()
+    at_85 = branches[0]
+    pass
 
