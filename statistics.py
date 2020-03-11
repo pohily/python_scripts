544bff0475d3bd458e8ad7e876149d1abebbb173
Михаил А. Похилый <m.pohilyj@pohilyj-ws.officeru.4slovo.ru>
2020-03-11 09:08:14 +0300

    is_merged проверяет не ветку задачи, а ветку, получаемую из МР - это могут быть разные ветки

--- statistics.py
+++ statistics.py
@@ -2,10 +2,10 @@ from configparser import ConfigParser
 from itertools import chain
 from re import sub
 
-from jira import JIRA
 import gitlab
+from jira import JIRA
 
-from build import get_release_details, get_merge_requests
+from build import get_release_details, get_merge_requests, PROJECTS_NUMBERS
 from send_notifications import STATUS_FOR_RELEASE
 
 """ Показывает статистику по запрошенному релизу: количество задач, проетов и названия проектов, 
@@ -18,17 +18,6 @@ PROJECTS_COUNTRIES = {7: "ru", 11: "kz", 12: "kz", 20: "ru", 22: "ru", 23: "ge",
                       128: "ge", 129: "ge", 130: "ru", 135: "ru, kz, ge", 138: "ru, kz, ge", 139: "ru, kz, ge",
                       144: "ru, kz, ge", 154: "ru", 159: "kz", 166: "ru", 167: "ru",
                       }
-PROJECTS_NUMBERS = {7: "4slovo.ru/chestnoe_slovo", 11: "4slovo.kz/crm4slovokz", 12: "4slovo.kz/4slovokz",
-                    20: "4slovo.ru/chestnoe_slovo_backend", 22: "4slovo.ru/common", 23: "mrloan.ge/mrloange",
-                    24: "mrloan.ge/crmmrloange", 61: "4slovo.ru/fias", 62: "4slovo.ru/chestnoe_slovo_landing",
-                    79: "4slovo.ru/api", 86: "4slovo/cache", 90: "4slovo/sawmill", 91: "4slovo/common",
-                    92: "4slovo/inn", 93: "4slovo/finance", 94: "docker/finance", 97: "docker/api", 100: "docker/ge",
-                    103: "4slovo/finance_client", 110: "docker/kz", 113: "4slovo/rabbitclient", 116: "4slovo/fs-client",
-                    117: "4slovo/fs", 121: "4slovo/enum-generator", 125: "4slovo/expression", 128: "almal.ge/almalge",
-                    129: "almal.ge/crmalmalge", 130: "4slovo.ru/python-tests", 135: "4slovo/logging",
-                    138: "4slovo/timeservice", 139: "4slovo/timeservice_client", 144: "docker/replicator",
-                    154: "4slovo.ru/python-scripts", 159: "4slovo.kz/landing", 166: "docker/ru",167: "docker/ru-db",
-                    }
 
 config = ConfigParser()
 config.read('config.ini')
