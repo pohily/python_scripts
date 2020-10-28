82255c612e7e0f938cada594195c0b567d4a8504
Michael Pohily <m.pohilyj@4slovo.ru>
2020-10-28 18:06:01 +0300

    деплой логи для 2 стефджинга

---
+++ deploy_logs2.py
@@ -0,0 +1,65 @@
+from configparser import ConfigParser
+from datetime import datetime
+
+import paramiko
+from jira import JIRA
+
+from build import get_merge_requests
+from constants import PROJECTS_NUMBERS, JIRA_SERVER, SYSTEM_USERS, COUNTRIES_ABBR
+from send_notifications import get_release_details
+
+""" Показывает деплой логи всех измененных в релизе проектов для 2 стейджингов"""
+
+
+def main():
+    config = ConfigParser()
+    config.read('config.ini')
+    jira_options = {'server': JIRA_SERVER}
+    jira = JIRA(options=jira_options, auth=(config['user_data']['login'], config['user_data']['jira_password']))
+    _, release_input, release_country, fix_issues, _ = get_release_details(config, jira)
+    release_country = COUNTRIES_ABBR[release_country]
+    used_projects = set()
+    print(f'\033[34m Выбираем проекты релиза {release_input}\033[0m')
+    for issue_number in fix_issues:
+        MR_count = get_merge_requests(config, issue_number, return_merged=True)
+        for merge in MR_count:
+            used_projects.add(merge.project)
+
+    projects = [PROJECTS_NUMBERS[pr] for pr in used_projects]
+
+    username = config['staging'][f"user_{release_country}"]
+    password = config['staging'][f"staging_password_{release_country}"]
+
+    if release_country == 'ru':
+        server = 'v2.staging.4slovo.ru'
+    else:
+        server = 'kz-v2.staging.4slovo.ru'
+
+    release_country = f'{release_country}2'
+    system_users = [SYSTEM_USERS[release_country][pr] for pr in projects]
+    system_users = [user for user in system_users if user]
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port=22, username=username, password=password, )
+
+    today = datetime.now().strftime("%Y-%m-%d")
+    # меня пока не пускают в этих юзеров 28.10.20
+    # if 'ru_frontend_new' in system_users:
+    #     system_users += ['ru_lk', 'ru_partner']
+    for user in system_users:
+        cmd = f"sudo -Siu {user} awk '/{today}/ ? ++i : i' logs/deploy.log"
+        _, ssh_stdout, stderr = client.exec_command(cmd)
+        err = stderr.read().decode('utf-8').strip("\n")
+        if err:
+            print(f'!!!!!!! Error in {user}: {err}')
+        else:
+            result = ssh_stdout.read().decode('utf-8').strip("\n")
+            print(f'{user} ================ Ok. {result}')
+        print(f'\033[34m Деплой лог для {user}\033[0m')
+        input('Нажмите Enter для продолжения')
+    client.close()
+
+
+if __name__ == '__main__':
+    main()
