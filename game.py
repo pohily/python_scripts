08ed202f01042c2c1caef4f4da2bae7d5c00ab4e
Michael Pohily <m.pohilyj@4slovo.ru>
2021-06-01 17:49:25 +0300

    поправил получение списка задач

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
+import datetime
 import sqlite3
-from _datetime import datetime
 from configparser import ConfigParser
 
 import gitlab
@@ -9,14 +9,13 @@ from constants import JIRA_SERVER, TESTERS
 
 
 def main(month):
-    with sqlite3.connect(f'game{datetime.now().strftime("%y")}{month}{datetime.now().strftime("%d")}.db') as connection:
+    with sqlite3.connect(f'game{datetime.datetime.now().strftime("%y%m%d")}-{month}.db') as connection:
         cursor = connection.cursor()
         cursor.execute("create table data ("
-                       "_id integer primary key autoincrement, "
+                       "id integer primary key autoincrement, "
                        "issue text not null, "
-                       "release text,"
+                       "release_name text,"
                        "release_date text not null, "
-                       "creator text, "
                        "tester_name text not null,"
                        "action text, "
                        "creation_point numeric default 0, "
@@ -30,18 +29,28 @@ def main(month):
         config.read('config.ini')
         jira_options = {'server': JIRA_SERVER}
         jira = JIRA(options=jira_options, auth=(config['user_data']['login'], config['user_data']['jira_password']))
-        start = f'{datetime.now().strftime("%Y")}-{month}-01'
-        finish = f'{datetime.now().strftime("%Y")}-{month+1}-01'
-        issues = jira.search_issues(
-            f'status = "Released to production" and updated >= "{start}" and updated < "{finish}"'
-        )
-        delta = 0
-        for index, issue in enumerate(issues):
-            index = index - delta
-            releasedate = datetime.strptime(issue.fields.fixVersions[0].releaseDate, '%Y-%m-%d')
-            if datetime.strptime(start, '%Y-%m-%d') > releasedate or releasedate > datetime.strptime(finish, '%Y-%m-%d'):
-                delta += 1
-                continue
+        start = f'{datetime.datetime.now().strftime("%Y")}-{month}-01'
+        date = datetime.datetime.strptime(start, '%Y-%m-%d')
+        finish = datetime.datetime.strptime(f'{datetime.datetime.now().strftime("%Y")}-{month + 1}-01', '%Y-%m-%d')
+        # сперва ищем названия всех зарелиженных релизов
+        fixes = set()
+        while date < finish:
+            next_day = date + datetime.timedelta(days=1)
+            tmp = jira.search_issues(
+                f'status = "Released to production" and resolved >= "{date.strftime("%Y-%m-%d")}" '
+                f'and resolved < "{next_day.strftime("%Y-%m-%d")}"'
+            )
+            if tmp:
+                for i in tmp:
+                    fixes.add(i.fields.fixVersions[0].name)
+            date = next_day
+        # теперь ищем все задачи из найденных релизов
+        issues = []
+        for fix in fixes:
+            issues += jira.search_issues(f'fixVersion={fix}')
+        # заполняем БД задачами SLOV
+        index = 0
+        for issue in issues:
             if issue.fields.creator.name in TESTERS:
                 creator = issue.fields.creator.displayName
             else:
@@ -52,8 +61,7 @@ def main(month):
                     f"'{issue.fields.fixVersions[0].name}'," \
                     f"'{issue.fields.fixVersions[0].releaseDate}'," \
                     f"'{creator}'," \
-                    f"'{issue.fields.assignee.displayName}'," \
-                    f"'стейджинг'," \
+                    f"''," \
                     f"'', '', '1', '1', '', '', '')"
             cursor.execute(query)
             index += 1
@@ -62,7 +70,7 @@ def main(month):
         project = gl.projects.get(130)
         mrs = project.mergerequests.list(state='merged', target_branch='master')
         for mr in mrs:
-            if datetime.strptime(mr.attributes['merged_at'].split('T')[0], '%Y-%m-%d').month == month:
+            if datetime.datetime.strptime(mr.attributes['merged_at'].split('T')[0], '%Y-%m-%d').month == month:
                 for action in ['создание', 'разработка', 'ревью']:
                     if action == 'разработка':
                         creator = mr.attributes['author']['name']
@@ -74,17 +82,15 @@ def main(month):
                             f"''," \
                             f"'{mr.attributes['merged_at'].split('T')[0]}'," \
                             f"'{creator}'," \
-                            f"''," \
                             f"'{action}'," \
                             f"'', '', '1', '1', '', '', '')"
                     cursor.execute(query)
                     index += 1
             else:
-                break
+                continue
         connection.commit()
 
 
 if __name__ == '__main__':
     month = 5
     main(month)
-
