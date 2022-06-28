d9a976bc4c7ac34c95496db275dc182f0e16a9b1
Михаил Похилый <m.pohilyj@fairtech.ru>
2022-06-28 15:10:09 +0300

    в game сделал проверку на количество реквестов в автотестах, т.к. гитлаб отдает только 20 последних

--- constants.py
+++ constants.py
@@ -106,7 +106,8 @@ PRIORITY = {'Critical': '(*r) - Critical', 'Highest': '(!) - Highest', 'High': '
             'MEGA Critical': '(flag) - MEGA Critical'}
 STATUS_FOR_RELEASE = ['MEGA Critical', 'Released to production', 'Passed QA', 'In regression test', 'Ready for release',
     'Закрыт', 'Fixed', 'Closed', 'Готово'
-    ]#, 'Ready for review', 'Ready for technical solution review', 'In QA', 'Open', 'Ready for QA', 'In development', 'Reopened', 'Reviewing', 'Technical solution' ]
+    ]#, 'Ready for review', 'Ready for technical solution review', 'In QA', 'Open', 'Ready for QA',
+     # 'In development', 'Reopened', 'Reviewing', 'Technical solution' ]
 STATUS_READY = ['Released to production', 'Ready for release', 'Закрыт', 'Fixed', 'Closed']
 
 TESTERS = {
